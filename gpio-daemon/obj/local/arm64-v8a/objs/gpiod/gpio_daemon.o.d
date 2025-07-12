ea5b38a76c5cbe3759043ee99865ade382873d59
RamkumarMohan6383 <95962773+RamkumarMohan6383@users.noreply.github.com>
2025-07-12 17:17:30 +0530

    V1.01 With Hardware PWM

---
+++ gpio-daemon/obj/local/arm64-v8a/objs/gpiod/gpio_daemon.o.d
@@ -0,0 +1,3 @@
+./obj/local/arm64-v8a/objs/gpiod/gpio_daemon.o: jni/gpio_daemon.c \
+  jni/gpio_mmio.h
+jni/gpio_mmio.h:
---
+++ gpio-daemon/obj/local/arm64-v8a/objs/gpiod/gpio_mmio.o.d
@@ -0,0 +1,3 @@
+./obj/local/arm64-v8a/objs/gpiod/gpio_mmio.o: jni/gpio_mmio.c \
+  jni/gpio_mmio.h
+jni/gpio_mmio.h:
